6addcad328c782dbb38cf7487fa7029107e29cc7
Aditya Deshmukh <adityad1405@gmail.com>
2023-09-04 21:03:54 +0530

    Added buttons and implemented DOM Methods

--- game.js
+++ game.js
@@ -1,39 +1,61 @@
 function getComputerChoice() {
-   let computerChoice = ['rock', 'paper', 'scissors']
+   const computerChoice = ['rock', 'paper', 'scissors']
    return computerChoice[Math.floor(Math.random()*3)]
 }   
 
-const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-
-console.log("The Computer chose:", computerSelection);
-console.log("The Player chose:", playerSelection);
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection) {
-        console.log("It's a Tie!");
+        result.appendChild(itsATie);
     }
     else if(playerSelection == "rock" && computerSelection == "paper"){
-        console.log("Computer Wins!")
+        result.append(computerWins)
     }
     else if(playerSelection == "rock" && computerSelection == "scissors"){
-        console.log("Player Wins!")
+        result.append(playerWins)
     }
     else if(playerSelection == "paper" && computerSelection == "rock"){
-        console.log("Player Wins!")
+        result.append(playerWins)
     }
     else if(playerSelection == "paper" && computerSelection == "scissors"){
-        console.log("Computer Wins!")
+        result.append(computerWins)
     }
     else if(playerSelection == "scissors" && computerSelection == "paper"){
-        console.log("Player Wins!")
+        result.append(playerWins)
     }
     else if(playerSelection == "scissors" && computerSelection == "rock"){
-        console.log("Computer Wins!")
+        result.append(computerWins)
     }
 }
+//console.log("The Computer chose:", computerSelection);
+//console.log("The Player chose:", playerSelection);
 
-playRound(playerSelection, computerSelection);
+
+//playRound(playerSelection, computerSelection);
+
+const button1 = document.querySelector('#rock');
+button1.addEventListener('click', function() {playRound("rock", computerSelection)});
+
+const button2 = document.querySelector('#paper');
+button2.addEventListener('click', function () {playRound("paper", computerSelection)});
+
+const button3 = document.querySelector('#scissors');
+button3.addEventListener('click', function() {playRound("scissors", computerSelection)});
+
+const result = document.querySelector('.result');
+const h2 = document.createElement('h2');
 
 
 
+const playerWins = document.createElement('h3');
+const computerWins = document.createElement('h3');
+const itsATie = document.createElement('h3');
+
+playerWins.textContent = "Player wins";
+computerWins.textContent = "Computer wins";
+itsATie.textContent = "It's a Tie";
+
+h2.textContent = "The winner is: ";
+result.appendChild(h2);
+//console.log(playerSelection);
